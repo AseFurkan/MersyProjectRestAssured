7fa78eddab50413e2c639d2251ef31f0e3b76a9c
mustafayumrutepe <mstfyumrutepe@gmail.com>
2023-11-12 02:45:01 +0300

    mission completed

---
+++ src/test/java/Fields/Field.java
@@ -0,0 +1,15 @@
+package Fields;
+
+public class Field {
+
+    public String id;
+    public String name;
+    public Object[] translateName;
+    public String code ;
+    public String type ="DATE";
+    public Object[] children;
+    public boolean systemField =false;
+    public String systemFieldName;
+    public String schoolId="646cbb07acf2ee0d37c6d984";
+
+}
